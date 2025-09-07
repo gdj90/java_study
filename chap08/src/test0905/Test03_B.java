100113e966a01564096a391d095f00b0be85f679
kmsclass <kms@goodee.co.kr>
2025-09-08 08:45:49 +0900

    2025-09-08 0845

---
+++ chap08/src/test0905/Test03_B.java
@@ -0,0 +1,43 @@
+package test0905;
+/*
+ * 구동클래스를 실행하였을때 다음의 결과가 나오도록
+ * 관련 클래스를 구현하기
+ [결과]  
+DanceRobot은 춤을 춤니다.
+SingRobot은 노래를 부릅니다.
+DrawRobot은 그림을 그립니다.
+ */
+/*
+abstract class Robot {
+	abstract void action();
+}
+class DanceRobot extends Robot {
+	@Override
+	public void action() {
+		System.out.println("DanceRobot은 춤을 춤니다.");
+	}
+}
+class SingRobot extends Robot {
+	@Override
+	public void action() {
+		System.out.println("SingRobot은 노래를 부릅니다.");
+	}
+}
+class DrawRobot extends Robot {
+	@Override
+	public void action() {
+		System.out.println("DrawRobot은 그림을 그립니다.");
+	}
+}
+*/
+public class Test03_B {
+	public static void main(String[] args) {
+		Robot[] robot = new Robot[3];
+		robot[0] = new DanceRobot();
+		robot[1] = new SingRobot();
+		robot[2] = new DrawRobot();
+		for(Robot r : robot) {
+			r.action(); 
+		}
+	}
+}
