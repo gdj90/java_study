261ed91ea1d120c959b9ff07b745ec46fd733b5a
kmsclass <kms@goodee.co.kr>
2025-09-19 08:45:10 +0900

    2025-09-19 0845

---
+++ chap15_thread/src/test0918/Test01_A.java
@@ -0,0 +1,51 @@
+package test0918;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.util.Vector;
+
+/*
+D:/20250825/java/java_study/chap13_iostream/src/ : 각자의 chap13 폴더
+
+D:/20250825/java/java_study/chap13_iostream/src/폴더 하위 폴더의 .java 파일 중
+Exam 관련 모든 소스 내용을  자바IO실습예제.txt  파일 하나에 저장 하기
+
+Exam 관련 모든 소스 : ExamXXX.java
+*/
+
+public class Test01_A {
+	
+	static Vector<InputStream> vector = new Vector();
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("프로그램 시작");
+		File f1 = new File("D:/20250825/java/java_study/chap13_iostream/src/");
+		dirSearch(f1,f1.list()); 
+		int len;
+		FileOutputStream fos = new FileOutputStream("자바IO실습예제.txt");
+		SequenceInputStream sis = new SequenceInputStream(vector.elements());
+		byte[] buf = new byte[8096]; 
+		while((len=sis.read(buf)) != -1) {
+			fos.write(buf,0,len);
+	    }
+		fos.flush();
+		fos.close();
+		sis.close();
+		System.out.println("프로그램 종료");
+    }
+	private static void dirSearch(File f,String[] dir) throws IOException {
+		for(String file : dir) {
+			File f2 = new File(f,file);
+			if(f2.isDirectory()) { //폴더. 
+				dirSearch(f2,f2.list()); //재귀함수.하위목록
+		    } else {
+		    	if(file.contains("Exam"))
+		    	   vector.add(new FileInputStream(f2)); 
+		    }
+		}		
+	}
+}
---
+++ chap15_thread/src/test0918/Test02_A.java
@@ -0,0 +1,41 @@
+package test0918;
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntPredicate;
+
+/*
+ * 1에서 100까지의 임의의 수 10개를 저장한 배열 arr에서
+ * IntBinaryOperator 인터페이스와 IntPredicate 인터페이스를 이용하여
+ * 배열에 저장된 짝수,홀수별 최대 숫자와 최소 숫자를 출력하기
+ */
+public class Test02_A {
+	private static int[] arr = new int[10];
+	public static void main(String[] args) {
+		for(int i=0; i<arr.length; i++) {
+			arr[i]= (int)(Math.random()*100)+1;
+ 		    System.out.print(arr[i]+",");
+		}
+		System.out.println();
+		System.out.println("홀수 최대값:"
+		+maxOrMin((a,b)->(a>=b)?a:b,a->a%2!=0));
+		System.out.println("짝수 최대값:"
+		+maxOrMin((a,b)->(a>=b)?a:b,a->a%2==0));
+		System.out.println("홀수 최소값:"
+		+maxOrMin((a,b)->(a<=b)?a:b,a->a%2!=0));
+		System.out.println("짝수 최소값:"
+		+maxOrMin((a,b)->(a<=b)?a:b,a->a%2==0));
+		System.out.println("최대값:"
+		+maxOrMin((a,b)->(a>=b)?a:b,a->true));
+		System.out.println("최소값:"
+		+maxOrMin((a,b)->(a<=b)?a:b,a->true));
+	}
+	private static int maxOrMin
+	(IntBinaryOperator op, IntPredicate p) {
+		int result = 0;
+		for(int a : arr) {
+			if (p.test(a)) //짝수 또는 홀수
+				if(result == 0) result = a; //첫번째 홀수
+				else result = op.applyAsInt(result, a);
+		}
+		return result;
+	}
+}
